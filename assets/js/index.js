0f41f0558bc17ccfdbcbeb11f2c04be21e224215
Michael Rao <jmichaelrao@gmail.com>
2021-05-29 11:46:48 -0700

    fix navbar sizing

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,9 @@ $(function () {
   $(".icon").hover(
     function () {
       $(this).children("p").toggleClass("d-none");
-      $(this).children("a").addClass("d-none");
     },
     function () {
       $(this).children("p").toggleClass("d-none");
-      $(this).children("a").removeClass("d-none");
     }
   );
 });
